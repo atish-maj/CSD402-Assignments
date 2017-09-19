39458881b0e4f7c4bafb4f0768b0f3bde9d14aa6
atish-maj <am533@snu.edu.in>
2017-09-19 11:46:28 +0530

    calendar

---
+++ Lab6- Calendar/js/events.js
@@ -0,0 +1,191 @@
+var events = {
+	"user1" : [
+	{
+		"id" : 1,
+		"title": "Event1",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Budday",
+		"day": "Tuesday"
+	},
+	{
+		"id" : 1,
+		"title": "Party",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Sharty",
+		"day": "Thursday"
+	},
+	{
+		"id" : 1,
+		"title": "Apple Sorbet",
+		"day": "Friday"
+	},
+	{
+		"id" : 1,
+		"title": "Gooble Gooble",
+		"day": "Saturday"
+	},
+	{
+		"id" : 1,
+		"title": "Event2s",
+		"day": "Sunday"
+	},
+	],
+
+	"user2" : [
+	{
+		"id" : 1,
+		"title": "Event1",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Budday",
+		"day": "Tuesday"
+	},
+	{
+		"id" : 1,
+		"title": "Party",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Sharty",
+		"day": "Thursday"
+	},
+	{
+		"id" : 1,
+		"title": "Apple Sorbet",
+		"day": "Friday"
+	},
+	{
+		"id" : 1,
+		"title": "Gooble Gooble",
+		"day": "Saturday"
+	},
+	{
+		"id" : 1,
+		"title": "Event2s",
+		"day": "Sunday"
+	},
+	],
+
+	"user5" : [
+	{
+		"id" : 1,
+		"title": "Event1",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Budday",
+		"day": "Tuesday"
+	},
+	{
+		"id" : 1,
+		"title": "Party",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Sharty",
+		"day": "Thursday"
+	},
+	{
+		"id" : 1,
+		"title": "Apple Sorbet",
+		"day": "Friday"
+	},
+	{
+		"id" : 1,
+		"title": "Gooble Gooble",
+		"day": "Saturday"
+	},
+	{
+		"id" : 1,
+		"title": "Event2s",
+		"day": "Sunday"
+	},
+	],
+
+	"user3" : [
+	{
+		"id" : 1,
+		"title": "Event1",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Budday",
+		"day": "Tuesday"
+	},
+	{
+		"id" : 1,
+		"title": "Party",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Sharty",
+		"day": "Thursday"
+	},
+	{
+		"id" : 1,
+		"title": "Apple Sorbet",
+		"day": "Friday"
+	},
+	{
+		"id" : 1,
+		"title": "Gooble Gooble",
+		"day": "Saturday"
+	},
+	{
+		"id" : 1,
+		"title": "Event2s",
+		"day": "Sunday"
+	},
+	],
+
+	"user4" : [
+	{
+		"id" : 1,
+		"title": "Event1",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Budday",
+		"day": "Tuesday"
+	},
+	{
+		"id" : 1,
+		"title": "Party",
+		"day": "Monday"
+	},
+	{
+		"id" : 1,
+		"title": "Sharty",
+		"day": "Thursday"
+	},
+	{
+		"id" : 1,
+		"title": "Apple Sorbet",
+		"day": "Friday"
+	},
+	{
+		"id" : 1,
+		"title": "Gooble Gooble",
+		"day": "Saturday"
+	},
+	{
+		"id" : 1,
+		"title": "Event2s",
+		"day": "Sunday"
+	},
+	]
+}
